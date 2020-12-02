f78bddde97d39c7549167f2779c7c95a6a5ba850
Artem Mantii <artem_mantii@icloud.om>
2020-12-03 01:27:11 +0300

    Add HW#1

---
+++ HW. Task 1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string arg1 = args[0];
+            int intArg1 = int.Parse(arg1);
+
+            string arg2 = args[1];
+            int intArg2 = int.Parse(arg2);
+
+            Console.WriteLine("Enter the first value");
+            string value1 = Console.ReadLine();
+            int a = int.Parse(value1);
+
+            Console.WriteLine("Enter the second value");
+            string value2 = Console.ReadLine();
+            int b = int.Parse(value2);
+
+            int c = a + b + intArg1 + intArg2;
+            Console.Write(value1 + "+" + value2 + "+" + intArg1 + "+" + intArg2 + "=" + c);
+            
+
+           
+        }
+    }
+}
---
+++ HW. Task 1/ConsoleApp1/HomeApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HomeApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what's your name?");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Nice to meet you," + name + "!");
+
+            Console.WriteLine("Tell me the first number.");
+            string num1 = Console.ReadLine();
+            int a = int.Parse(num1);
+
+            Console.WriteLine("And now, tell me please the second number.");
+            string num2 = Console.ReadLine();
+            int b = int.Parse(num2);
+
+            int c = a * b;
+            Console.WriteLine(num1 + "*" + num2 + "=" + c);
+
+            Console.Write("The resault of multiplying of your numbers is" + " " + c + ".");
+
+
+        }
+    }
+}
