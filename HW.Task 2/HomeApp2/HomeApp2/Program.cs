f75aea17f34e19d49582d65cef71313d5f8e8b7f
Artem Mantii <artem_mantii@icloud.om>
2020-12-05 11:59:01 +0300

    HW#3. Task 1

--- HW.Task 2/HomeApp2/HomeApp2/Program.cs
+++ HW.Task 2/HomeApp2/HomeApp2/Program.cs
@@ -6,15 +6,138 @@ namespace HomeApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Do you take antibiotics? (YES - 1, NO - 0)");
-            string value1 = Console.ReadLine();
-            int intValue1 = int.Parse(value1);
-            bool antibiotics = intValue1 > 0;
+            bool buyAlckohol = true;
+
+            bool antibiotics = true;
+
+            bool adulthood = true;
+
+            bool passport = true;
+
+            bool driverLicense = true;
+
+            bool minCash100uah = true;
+
+            bool paymentCard = true;
+
+
+            bool isInputValid = false;
+            while (!isInputValid) 
+            {
+                Console.WriteLine("Do you take antibiotics? (YES - 1, NO - 0)");
+                string value1 = Console.ReadLine();
+                if (value1 == "1")
+                {
+                    antibiotics = true;
+                    isInputValid = true;
+                }
+                else if (value1 == "0")
+                {
+                    adulthood = false;
+                    isInputValid = true;
+                }
+
+                buyAlckohol = !antibiotics;
+
+                Console.WriteLine("Do you over 21 years old? (YES - 1, NO - 0)");
+                string value2 = Console.ReadLine();
+                if (value2 == "1")
+                {
+                    adulthood = true;
+                    isInputValid = true;
+                }
+                else if (value2 == "0")
+                {
+                    adulthood = false;
+                    isInputValid = true;
+                }
+
+                buyAlckohol = adulthood;
+
+                Console.WriteLine("Do you have passport? (YES - 1, NO - 0)");
+                string value3 = Console.ReadLine();
+                if (value3 == "1")
+                {
+                    passport = true;
+                    isInputValid = true;
+                }
+                else if (value3 == "0") 
+                {
+                    passport = false;
+                    isInputValid = true;
+                }
+
+                if (!passport)
+                {
+                    Console.WriteLine("Do you have Driver License? (YES - 1, NO - 0)");
+                    string value4 = Console.ReadLine();
+                    if (value4 == "1")
+                    {
+                        driverLicense = true;
+                        isInputValid = true;
+                    }
+                    else if (value4 == "0")
+                    {
+                        driverLicense = false;
+                        isInputValid = true;
+                    }
+                    buyAlckohol = passport || driverLicense;
+                }
+
+                Console.WriteLine("Do you have cash more than 100 UAH? (YES - 1, NO - 0)");
+                string value5 = Console.ReadLine();
+                if (value5 == "1")
+                {
+                    minCash100uah = true;
+                    isInputValid = true;
+                }
+                else if (value5 == "0")
+                {
+                    minCash100uah = false;
+                    isInputValid = true;
+                }
+                if (!minCash100uah)
+                {
+                    Console.WriteLine("Do you have payment card? (YES - 1, NO - 0)");
+                    string value6 = Console.ReadLine();
+                    if (value6 == "1")
+                    {
+                        paymentCard = true;
+                        isInputValid = true;
+                    }
+                    else if (value6 == "0")
+                    {
+                        paymentCard = false;
+                        isInputValid = true;
+                    }
+                    buyAlckohol = minCash100uah || paymentCard;
+                }
+                Console.WriteLine(buyAlckohol);
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+        }
+
+           
+            
+                
+           
 
             
-            Console.WriteLine("Do you over 21 years old? (YES - 1, NO - 0)");
-            string value2 = Console.ReadLine();
-            int intValue2 = int.Parse(value2);
+            /*Console.WriteLine("Do you over 21 years old? (YES - 1, NO - 0)");
+            string value20 = Console.ReadLine();
+            int intValue20 = int.Parse(value20);
             bool adulthood = intValue2 > 0;
 
             Console.WriteLine("Do you have passport? (YES - 1, NO - 0)");
@@ -39,8 +162,7 @@ namespace HomeApp2
 
             bool buyAlcohol = !antibiotics && adulthood && (passport || driverLicense) && (minCash100uah || paymentCard);
 
-            Console.WriteLine(buyAlcohol);
+            Console.WriteLine(buyAlcohol);*/
             
         }
     }
-}
