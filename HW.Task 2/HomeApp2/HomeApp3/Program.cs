6ef7586528bba9052334c519547b1b272b193c64
Alexander Skrinnik <askrinnik@amtoss.com.ua>
2020-12-05 07:56:56 -0500

    A loop was added to HomeApp3

--- HW.Task 2/HomeApp2/HomeApp3/Program.cs
+++ HW.Task 2/HomeApp2/HomeApp3/Program.cs
@@ -2,47 +2,57 @@
 
 namespace HomeApp3
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      Console.WriteLine("Hello, I'm a dictionary. I can translate some words about the weather.");
+
+      Boolean readyToExit = false;
+      while (!readyToExit)
+      {
+        string enteredWord = Console.ReadLine();
+        switch (enteredWord)
         {
-            Console.WriteLine("Hello, I'm a dictionary. I can translate some words about the weather.");
-            string enteredWord = Console.ReadLine();
-            switch (enteredWord)
-            {
-                case "synnu":
-                    Console.WriteLine("Солнечно");
-                    break;
+          case "sunny":
+            Console.WriteLine("Солнечно");
+            break;
 
-                case "cloudy":
-                    Console.WriteLine("Облачно");
-                    break;
+          case "cloudy":
+            Console.WriteLine("Облачно");
+            break;
 
-                case "windy":
-                    Console.WriteLine("Ветрено");
-                    break;
+          case "windy":
+            Console.WriteLine("Ветрено");
+            break;
 
-                case "hazy":
-                    Console.WriteLine("Туманно");
-                    break;
+          case "hazy":
+            Console.WriteLine("Туманно");
+            break;
 
-                case "warmly":
-                    Console.WriteLine("Тепло");
-                    break;
+          case "warmly":
+            Console.WriteLine("Тепло");
+            break;
 
-                case "frosty":
-                    Console.WriteLine("Холодно");
-                    break;
+          case "frosty":
+            Console.WriteLine("Холодно");
+            break;
 
-                case "rainy":
-                    Console.WriteLine("Дождливо");
-                    break;
+          case "rainy":
+            Console.WriteLine("Дождливо");
+            break;
 
-                default:
-                    Console.WriteLine("You have entered unknown word!");
-                    break;
+          case "exit":
+            readyToExit = true;
+            break;
 
-            }
+          default:
+            Console.WriteLine("You have entered unknown word!");
+            break;
+
         }
+
+      }
     }
+  }
 }
