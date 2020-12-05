179af2c97d04778195aed6866aa268aae249cfd3
Alexander Skrinnik <askrinnik@amtoss.com.ua>
2020-12-05 08:50:42 -0500

    Methods were used in the program

--- HW.Task 2/HomeApp2/HomeApp2/Program.cs
+++ HW.Task 2/HomeApp2/HomeApp2/Program.cs
@@ -4,136 +4,62 @@ namespace HomeApp2
 {
   class Program
   {
+    static bool ReadBoolValueFromConsole(string message, string trueValue, string falseValue)
+    {
+      while (true)
+      {
+        OutputQuestion(message, trueValue, falseValue);
+        string inputValue = Console.ReadLine();
+        if (inputValue.ToLower() == trueValue)
+          return true;
+        else if (inputValue.ToLower() == falseValue)
+          return false;
+      }
+    }
+    static bool ReadBoolValueFromConsole(string message)
+    {
+      return ReadBoolValueFromConsole(message, "y", "n");
+    }
+
+    static int ReadIntValueFromConsole(string message)
+    {
+      while(true)
+      {
+        Console.WriteLine(message);
+        string inputValue = Console.ReadLine();
+
+        int intValue;
+        bool validValue = int.TryParse(inputValue, out intValue);
+
+        if (validValue)
+          return intValue;
+      }
+    }
+
+    static void OutputQuestion(string message, string trueValue, string falseValue)
+    {
+      Console.WriteLine(message + " (" + trueValue + "/" + falseValue + ")");
+    }
+
     static void Main(string[] args)
     {
-      bool antibiotics = true;
-
-      bool adulthood = true;
-
-      bool passport = true;
-
-      bool driverLicense = true;
-
-      bool minCash100uah = true;
-
-      bool paymentCard = true;
-
-      bool isInputValid = false;
-      while (!isInputValid)
-      {
-        Console.WriteLine("Do you take antibiotics? (YES - 1, NO - 0)");
-        string value1 = Console.ReadLine();
-        if (value1 == "1")
-        {
-          antibiotics = true;
-          isInputValid = true;
-        }
-        else if (value1 == "0")
-        {
-          antibiotics = false;
-          isInputValid = true;
-        }
-      }
-
+      bool antibiotics = ReadBoolValueFromConsole("Do you take antibiotics?");
       bool buyAlcohol = !antibiotics;
 
-      isInputValid = false;
-      while (!isInputValid)
-      {
-        Console.WriteLine("Do you over 21 years old? (y/n)");
-        string value2 = Console.ReadLine();
-        if (value2.ToLower() == "y")
-        {
-          adulthood = true;
-          isInputValid = true;
-        }
-        else if (value2.ToUpper() == "N")
-        {
-          adulthood = false;
-          isInputValid = true;
-        }
-      }
-
+      int age = ReadIntValueFromConsole("Do you over 21 years old?");
+      bool adulthood = age >= 21;
       buyAlcohol = buyAlcohol && adulthood;
 
-      isInputValid = false;
-      while (!isInputValid)
-      {
-        Console.WriteLine("Do you have passport? (y/n)");
-        string value3 = Console.ReadLine();
-        if (value3.ToLower() == "y")
-        {
-          passport = true;
-          isInputValid = true;
-        }
-        else if (value3.ToLower() == "n")
-        {
-          passport = false;
-          isInputValid = true;
-        }
-      }
-
-      driverLicense = false;
+      bool passport = ReadBoolValueFromConsole("Do you have passport?");
+      bool driverLicense = false;
       if (!passport)
-      {
-        isInputValid = false;
-        while (!isInputValid)
-        {
-          Console.WriteLine("Do you have Driver License? (YES - 1, NO - 0)");
-          string value4 = Console.ReadLine();
-          if (value4 == "1")
-          {
-            driverLicense = true;
-            isInputValid = true;
-          }
-          else if (value4 == "0")
-          {
-            driverLicense = false;
-            isInputValid = true;
-          }
-        }
-      }
-
+        driverLicense = ReadBoolValueFromConsole("Do you have Driver License?");
       buyAlcohol = buyAlcohol && (passport || driverLicense);
 
-      isInputValid = false;
-      while (!isInputValid)
-      {
-        Console.WriteLine("Do you have cash more than 100 UAH? (YES - 1, NO - 0)");
-        string value5 = Console.ReadLine();
-        if (value5 == "1")
-        {
-          minCash100uah = true;
-          isInputValid = true;
-        }
-        else if (value5 == "0")
-        {
-          minCash100uah = false;
-          isInputValid = true;
-        }
-      }
-
-      paymentCard = false;
+      bool minCash100uah = ReadBoolValueFromConsole("Do you have cash more than 100 UAH ?");
+      bool paymentCard = false;
       if (!minCash100uah)
-      {
-        isInputValid = false;
-        while (!isInputValid)
-        {
-          Console.WriteLine("Do you have payment card? (YES - 1, NO - 0)");
-          string value6 = Console.ReadLine();
-          if (value6 == "1")
-          {
-            paymentCard = true;
-            isInputValid = true;
-          }
-          else if (value6 == "0")
-          {
-            paymentCard = false;
-            isInputValid = true;
-          }
-        }
-      }
-
+        paymentCard = ReadBoolValueFromConsole("Do you have payment card?");
       buyAlcohol = buyAlcohol && (minCash100uah || paymentCard);
 
       Console.WriteLine(buyAlcohol);
