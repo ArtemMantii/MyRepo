513caf9d2dfe57f414f9a5fe816ac69702d88ed2
Artem Mantii <artem_mantii@icloud.om>
2020-12-05 12:38:34 +0300

    HW#3. Task 2. c1

---
+++ HW.Task 2/HomeApp2/HomeApp3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace HomeApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, I'm a dictionary. I can translate some words about the weather.");
+            string enteredWord = Console.ReadLine();
+            switch (enteredWord)
+            {
+                case "synnu":
+                    Console.WriteLine("Солнечно");
+                    break;
+
+                case "cloudy":
+                    Console.WriteLine("Облачно");
+                    break;
+
+                case "windy":
+                    Console.WriteLine("Ветрено");
+                    break;
+
+                case "hazy":
+                    Console.WriteLine("Туманно");
+                    break;
+
+                case "warmly":
+                    Console.WriteLine("Тепло");
+                    break;
+
+                case "frosty":
+                    Console.WriteLine("Холодно");
+                    break;
+
+                case "rainy":
+                    Console.WriteLine("Дождливо");
+                    break;
+
+                default:
+                    Console.WriteLine("You have entered unknown word!");
+                    break;
+
+            }
+        }
+    }
+}
